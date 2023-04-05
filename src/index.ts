c186daadebc66902d85bdba48de6be8469b7ceb5
Tom-Julux <42802270+Tom-Julux@users.noreply.github.com>
2023-04-05 15:56:30 +0200

    Simplify module

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,13 @@
-import type { StoreApi, PartialState, State } from "zustand";
-
-declare global {
-    interface globalThis {
-        __SHARED_ZUSTAND_USED_CHANNELS__: Set<string>;
-    }
-}
-
 export function isSupported() {
     return "BroadcastChannel" in globalThis;
 }
 
-export function share<T extends State, K extends keyof T>(
-    key: K,
-    api: StoreApi<T>,
+export function share(
+    key: string,
+    api: any,
     { ref = "shared-", initialize = false } = {}
 ): [() => void, () => void] {
     const channelName = ref + "-" + key.toString();
-    if (process.env.NODE_ENV != "production") {
-        if (!globalThis.__SHARED_ZUSTAND_USED_CHANNELS__) {
-            globalThis.__SHARED_ZUSTAND_USED_CHANNELS__ = new Set();
-        }
-        if (globalThis.__SHARED_ZUSTAND_USED_CHANNELS__.has(channelName)) {
-            console.warn(
-                `Two shared properties are using the channel "${channelName}". If you want to reuse a channel name make shure to free the channel by calling unshare() first.`
-            );
-            return;
-        }
-        globalThis.__SHARED_ZUSTAND_USED_CHANNELS__.add(channelName);
-    }
 
     let channel = new BroadcastChannel(channelName);
     let externalUpdate = false;
@@ -53,16 +33,13 @@ export function share<T extends State, K extends keyof T>(
         }
         externalUpdate = true;
         timestamp = evt.data.timestamp;
-        api.setState({ [key]: evt.data.state } as PartialState<T>);
+        api.setState({ [key]: evt.data.state });
     };
 
     const sync = () => channel.postMessage(undefined);
     const unshare = () => {
         channel.close();
         cleanup();
-        if (process.env.NODE_ENV != "production") {
-            globalThis.__SHARED_ZUSTAND_USED_CHANNELS__.delete(channelName);
-        }
     };
 
     // fetches any available state
